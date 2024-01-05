9d383f713779b91bd15c1dc91238ddd6cb51ed2c
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-05 15:33:40 +0100

    page redesign

---
+++ src/pages/Rooms.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MdSettings } from "react-icons/md";
+function Rooms() {
+  return (
+    <main>
+      <nav>
+        <img src="" alt="" />
+        <input placeholder="search rooms" type="text" />
+        <MdSettings size={25} />
+      </nav>
+    </main>
+  );
+}
+
+export default Rooms;
--- src/tesComponent/Footer.jsx
+++ src/tesComponent/Footer.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import { RiHome5Fill } from "react-icons/ri";
 import { FiSearch } from "react-icons/fi";
 import { LuLibrary } from "react-icons/lu";
+import { MdSettings } from "react-icons/md";
 function Footer() {
   return (
-    <foot className="fixed bottom-0 flex items-center justify-between px-6 bg-gray-800 w-screen rounded-t-[15px] h-[12vh] ">
+    <foot className="fixed bottom-0 text-[13px] flex items-center justify-between px-6 bg-gray-800 w-screen rounded-t-[15px] h-[12vh] ">
       <div className="flex flex-col  justify-center items-center gap-1">
-        <RiHome5Fill size={30} />
+        <RiHome5Fill size={25} />
         <p>Home</p>
       </div>
       <div className="flex flex-col justify-center items-center  gap-1">
-        <FiSearch size={30} />
+        <FiSearch size={25} />
         <p>search</p>
       </div>
       <div className="flex flex-col justify-center items-center  gap-1">
-        <LuLibrary size={30} />
+        <LuLibrary size={25} />
         <p>Your rooms</p>
+      </div>
+      <div className="flex flex-col justify-center items-center  gap-1">
+        <MdSettings size={25} />
+        <p>Settings</p>
       </div>
     </foot>
   );
