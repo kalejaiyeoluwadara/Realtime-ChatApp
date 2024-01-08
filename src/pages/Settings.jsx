1cca0a4f5dc39699993580ed5309a25d15eb0662
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-08 14:03:37 +0100

    settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { IoIosArrowBack } from "react-icons/io";
+import {
+  IoIosArrowBack,
+  IoIosArrowDown,
+  IoIosArrowDropright,
+} from "react-icons/io";
+import { IoMoonOutline } from "react-icons/io5";
 import { useGlobal } from "../context";
+import { MdLanguage, MdStyle } from "react-icons/md";
 import Footer from "../tesComponent/Footer";
+import { BsMoon, BsSun, BsBriefcase } from "react-icons/bs";
 
+const Switch = () => {
+  return (
+    <div className="h-[20px] w-[40px] shadow-md items-center rounded-[30px] bg-white flex justify-start relative ">
+      <div className="bg-blue-500 h-[23px] w-[23px] rounded-[50%] shadow-md absolute "></div>
+    </div>
+  );
+};
 function Settings() {
   const { isLight } = useGlobal();
   return (
     <div className="w-screen font-[poppins] h-screen bg-gray-900 ">
       <nav
-        className={`w-full flex justify-start items-center px-6 py-4 rounded-b-[20px] gap-6  bg-gray-800 h-[10vh] backdrop-filter backdrop-blur-md bg-opacity-30 ${
+        className={`w-full flex justify-start items-center px-6 py-4 rounded-b-[20px] gap-6   h-[10vh] backdrop-filter backdrop-blur-md bg-opacity-30 ${
           isLight ? "bg-white shadow-sm text-black " : "bg-gray-800 text-white"
         } `}
       >
@@ -19,14 +33,34 @@ function Settings() {
         <section className="mt-6 px-10">
           <h3 className="font-[600] text-[20px] ">Personalisation</h3>
           <div className="bg-black mt-2 px-6 py-4 flex flex-col gap-4 rounded-[10px]  ">
-            <div>
-              <p>Dark Mode</p>
+            <div className="flex justify-between items-center">
+              <section className="flex gap-2 justify-center items-center">
+                <IoMoonOutline size={25} />
+                <p className="font-[500">Dark Mode</p>
+              </section>
+              <Switch />
             </div>
-            <div>
-              <p>Appearance</p>
+            <div className="flex justify-between items-center">
+              <section className="flex gap-2 justify-center items-center">
+                <MdStyle size={30} />
+                <p className="font-[500">Appearance</p>
+              </section>
+              <section>
+                <IoIosArrowDown />
+              </section>
+              {/* <Switch /> */}
             </div>
-            <div>
-              <p>Language</p>
+
+            <div className="flex justify-between items-center">
+              <section className="flex gap-2 justify-center items-center">
+                <MdLanguage size={30} />
+                <p className="font-[500">Language</p>
+              </section>
+              <section className="flex gap-2 items-center">
+                <p>Engl</p>
+                <IoIosArrowDropright />
+              </section>
+              {/* <Switch /> */}
             </div>
           </div>
         </section>
