b2326b54377e4f87971aedabfd929b33daf935f4
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-10 23:59:44 +0100

    rooms update

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { MdSettings } from "react-icons/md";
+import { MdSettings, MdOutlineAdd } from "react-icons/md";
 import { useGlobal } from "../context";
 import Footer from "../tesComponent/Footer";
 import Room from "../tesComponent/Room";
+const Empty = () => {
+  return (
+    <div className="flex flex-col w-full items-center justify-center h-full  ">
+      <img
+        className="h-[300px] w-[300px] "
+        src="https://cdn3d.iconscout.com/3d/premium/thumb/empty-box-7328018-5980405.png?f=webp"
+        alt=""
+      />
+      <p className="font-[600]">You haven't joined any room yet.</p>
+    </div>
+  );
+};
 function Rooms() {
-  const { roompage, locRooms, setLocRooms, setPage } = useGlobal();
+  const { locRooms, setLocRooms, setPage, isLight } = useGlobal();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     // Load rooms from localStorage
     const roomsList = localStorage.getItem("roomlist");
@@ -13,17 +27,29 @@ function Rooms() {
     }
 
     // Delete the localStorage item "roomhist"
+    setLoading(false);
   }, []);
+
   return (
-    <main className="w-screen text-white min-h-screen bg-gray-900 ">
-      <nav className="fixed z-40 bg-gray-900  pb-4 border-b border-white border-opacity-5 gap-3 flex items-center  justify-between px-6 py-3 w-screen ">
+    <main
+      className={`w-screen ${
+        isLight ? "bg-gray-100 text-black" : "bg-gray-900 text-white  "
+      } min-h-screen  `}
+    >
+      <nav
+        className={`fixed z-40  ${
+          isLight ? "bg-white text-black" : "bg-gray-900 text-white  "
+        }  pb-4 border-b border-white border-opacity-5 gap-3 flex items-center  justify-between px-6 py-3 w-screen `}
+      >
         <img
           className="h-[40px] w-[40px] rounded-[50%] "
           src="https://images.unsplash.com/photo-1704072384017-788b1ccbaae5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <input
-          className="bg-gray-900 outline-none placeholder:font-[600] placeholder:mb-2  px-3 border border-white border-opacity-[0.1] rounded-[20px] w-[230px] "
+          className={` ${
+            isLight ? "bg-gray-100 text-black" : "bg-gray-900 text-white  "
+          } outline-none placeholder:font-[600] placeholder:mb-2  px-3 border border-white border-opacity-[0.1] rounded-[20px] w-[230px] `}
           placeholder="search rooms"
           type="text"
         />
@@ -35,11 +61,20 @@ function Rooms() {
         />
       </nav>
       <main className="mt-[75px] gap-2 mb-[120px] flex flex-col items-center ">
-        {locRooms.map((room, id) => {
-          return <Room name={room} />;
-        })}
+        {loading ? (
+          <p>Loading...</p>
+        ) : locRooms.length === 0 ? (
+          <Empty />
+        ) : (
+          locRooms.map((room, id) => <Room key={id} name={room} />)
+        )}
       </main>
       <Footer />
+
+      {/* create button */}
+      <div className="absolute h-[60px] w-[60px] rounded-[50%] flex items-center justify-center bottom-[100px] right-3 bg-blue-500 text-white ">
+        <MdOutlineAdd size={35} />
+      </div>
     </main>
   );
 }
