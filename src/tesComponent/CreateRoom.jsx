1b81704d3ee958b79a860b995ef2cf92ea7fe445
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-04 21:39:35 +0100

    trims

--- src/tesComponent/CreateRoom.jsx
+++ src/tesComponent/CreateRoom.jsx
@@ -1,71 +1,81 @@
-import React, { useState } from 'react';
-import { useGlobal } from '../context';
-import ChatRoom from './ChatRoom';
-import { TiArrowBack } from 'react-icons/ti';
+import React, { useState } from "react";
+import { useGlobal } from "../context";
+import ChatRoom from "./ChatRoom";
+import { TiArrowBack } from "react-icons/ti";
 import { IoEnterOutline } from "react-icons/io5";
-import { motion,AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 function CreateRoom() {
-  const {chat, setChat,room,setCreateRoom,isLight, setRoom,setPage} = useGlobal();
+  const { chat, setChat, room, setCreateRoom, isLight, setRoom, setPage } =
+    useGlobal();
 
   return (
     <motion.main
-    initial={{
-      opacity:0
-    }}
-    animate={{
-      opacity:1
-    }}
-    exit={{
-      opacity:0
-    }}
-    transition={{
-      duration:0.6,
-      delay:0.3
-    }}
-    className={`flex ${isLight ? "bo" : 'bg-black' } fixed top-0 z-50 left-0 text-white items-center justify-center h-screen w-screen`}>
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+      }}
+      transition={{
+        duration: 0.6,
+        delay: 0.3,
+      }}
+      className={`flex ${
+        isLight ? "bo" : "bg-black"
+      } fixed top-0 z-50 left-0 text-white items-center justify-center h-screen w-screen`}
+    >
       {!chat && (
         <motion.section
-        initial={{
-          y:"-100vh"
-        }}
-        animate={{
-          y:0
-        }}
-        exit={{
-          y:"-100vh"
-        }}
-        transition={{
-          duration:0.6
-        }}
-        className={`shadow-lg text-black  border-gray-300 w-[300px] rounded-[10px] gap-6 relative ${isLight ? 'bg-white border-2' :'bg-gray-900 text-white'} px-6 py-10 flex flex-col`}>
-          <TiArrowBack
-          onClick={() => {
-            setPage('general');
+          initial={{
+            y: "-100vh",
           }}
-          className='absolute  left-4 cursor-pointer '
-          size={25}
-        />
-          <h1 className='text-center font-[600] text-lg mb-4'>Create Chat Room</h1>
+          animate={{
+            y: 0,
+          }}
+          exit={{
+            y: "-100vh",
+          }}
+          transition={{
+            duration: 0.6,
+          }}
+          className={`shadow-lg text-black  border-gray-300 w-[300px] rounded-[10px] gap-6 relative ${
+            isLight ? "bg-white border-2" : "bg-gray-900 text-white"
+          } px-6 py-10 flex flex-col`}
+        >
+          <TiArrowBack
+            onClick={() => {
+              setPage("general");
+            }}
+            className="absolute  left-4 cursor-pointer "
+            size={25}
+          />
+          <h1 className="text-center font-[600] text-lg mb-4">
+            Create Chat Room
+          </h1>
           <input
-           className={`px-3 py-2 border text-black ${isLight ? 'bg-white':'bg-gray-800 border-opacity-25 text-white'} border-gray-400 rounded focus:outline-none focus:border-3 focus:border-blue-500`}
-           placeholder='Enter room name'
-          onChange={(e) => setRoom(e.target.value.toLowerCase())} type="text" />
+            className={`px-3 py-2 border text-black ${
+              isLight ? "bg-white" : "bg-gray-800 border-opacity-25 text-white"
+            } border-gray-400 rounded focus:outline-none focus:border-3 focus:border-blue-500`}
+            placeholder="Enter room name"
+            onChange={(e) => setRoom(e.target.value.toLowerCase().trim())}
+            type="text"
+          />
           <button
             onClick={() => {
-              
               setCreateRoom(false);
               setChat(true);
-              
-              console.log('hell yeahh');
+
+              console.log("hell yeahh");
             }}
-            className='px-4 py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600 flex items-center justify-center gap-2 text-center focus:outline-none'
+            className="px-4 py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600 flex items-center justify-center gap-2 text-center focus:outline-none"
           >
             Create <IoEnterOutline size={25} />
           </button>
         </motion.section>
       )}
-
-      
     </motion.main>
   );
 }
--- src/tesComponent/JoinRoom.jsx
+++ src/tesComponent/JoinRoom.jsx
@@ -1,57 +1,76 @@
-import React from 'react';
-import { useGlobal } from '../context';
-import { TiArrowBack } from 'react-icons/ti';
+import React from "react";
+import { useGlobal } from "../context";
+import { TiArrowBack } from "react-icons/ti";
 import { IoEnterOutline } from "react-icons/io5";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 function JoinRoom() {
-  const { nav, setNav, setPage,handleJoinClick, room, chat, setRoom, setChat, setJoinRoom,isLight } = useGlobal();
+  const {
+    nav,
+    setNav,
+    setPage,
+    handleJoinClick,
+    room,
+    chat,
+    setRoom,
+    setChat,
+    setJoinRoom,
+    isLight,
+  } = useGlobal();
 
   return (
     <motion.main
-    initial={{
-      opacity:0
-    }}
-    animate={{
-      opacity:1
-    }}
-    exit={{
-      opacity:0
-    }}
-    transition={{
-      duration:0.6
-    }}
-    className={`flex ${isLight ? "bo" : 'bg-black' } fixed top-0 z-50 left-0 text-white items-center justify-center h-screen w-screen`}>
-      <motion.section
-       initial={{
-        y:"-100vh"
+      initial={{
+        opacity: 0,
       }}
       animate={{
-        y:0
+        opacity: 1,
       }}
       exit={{
-        y:"-100vh"
+        opacity: 0,
       }}
       transition={{
-        duration:0.6
+        duration: 0.6,
       }}
-      className={`shadow-lg text-black  border-gray-300 w-[300px] rounded-[10px] gap-6 relative ${isLight ? 'bg-white border-2' :'bg-gray-900 text-white'} px-6 py-10 flex flex-col`}>
+      className={`flex ${
+        isLight ? "bo" : "bg-black"
+      } fixed top-0 z-50 left-0 text-white items-center justify-center h-screen w-screen`}
+    >
+      <motion.section
+        initial={{
+          y: "-100vh",
+        }}
+        animate={{
+          y: 0,
+        }}
+        exit={{
+          y: "-100vh",
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className={`shadow-lg text-black  border-gray-300 w-[300px] rounded-[10px] gap-6 relative ${
+          isLight ? "bg-white border-2" : "bg-gray-900 text-white"
+        } px-6 py-10 flex flex-col`}
+      >
         <TiArrowBack
           onClick={() => {
-            setPage('general');
+            setPage("general");
           }}
-          className='absolute  left-4 cursor-pointer '
+          className="absolute  left-4 cursor-pointer "
           size={25}
         />
-        <h1 className='text-center font-bold text-lg mb-4'>Enter Chat Room</h1>
+        <h1 className="text-center font-bold text-lg mb-4">Enter Chat Room</h1>
         <input
-         className={`px-3 py-2 border text-black ${isLight ? 'bg-white':'bg-gray-800 border-opacity-25 text-white'} border-gray-400 rounded focus:outline-none focus:border-3 focus:border-blue-500`}
-          placeholder='Enter room name'
-          type='text'
-          onChange={(e) => setRoom(e.target.value.toLowerCase())}
+          className={`px-3 py-2 border text-black ${
+            isLight ? "bg-white" : "bg-gray-800 border-opacity-25 text-white"
+          } border-gray-400 rounded focus:outline-none focus:border-3 focus:border-blue-500`}
+          placeholder="Enter room name"
+          type="text"
+          onChange={(e) => setRoom(e.target.value.toLowerCase().trim())}
         />
         <button
           onClick={handleJoinClick}
-          className='px-4 py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600 flex items-center justify-center gap-2 text-center focus:outline-none'
+          className="px-4 py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600 flex items-center justify-center gap-2 text-center focus:outline-none"
         >
           Join <IoEnterOutline size={25} />
         </button>
